8148c738c4d625eb65103a50236e8be16bb21ee8
waimea-jestevenson <jestevenson@waimea.school.nz>
2025-03-18 14:57:33 +1300

    Assessment setup changes

--- src/Main.kt
+++ src/Main.kt
@@ -2,12 +2,12 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Name:   Old Gold
+ * Project Author: Juliet Stevenson
+ * GitHub Repo:    https://github.com/waimea-jestevenson/level-2-programing--assessment
  * ---------------------------------------------------------------------
  * Notes:
- * PROJECT NOTES HERE
+ *
  * =====================================================================
  */
 
