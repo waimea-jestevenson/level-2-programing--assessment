53a48606b2fc1e6d21dd37c3ecee710d52e81040
waimea-jestevenson <jestevenson@waimea.school.nz>
2025-03-25 15:09:06 +1300

    Working on adding coins to the bored and having them in random places

--- src/Main.kt
+++ src/Main.kt
@@ -10,13 +10,17 @@
  *
  * =====================================================================
  */
-
+const val NUMCOINS = 5   //The Number of coins
 const val NUMSPACES = 10 //The total of spaces on the bored
-const val EMPTY ="---"  // Shows empty spaces on the bored
+const val EMPTY =" "  // Shows empty spaces on the bored
 fun main() {
     println("Old Gold!")
+    println("The objective of this game is to be the one to take the gold coin off the bored" +
+            "Players can do one of two things on their turn, they can either move a coin as many spaces to the left as is possible " +
+            "or they can take a coin off the bored if there is one in the left-most space.")
     //Creation of collection for the bored, and set up of the main game
-    val gameBored = mutableListOf<Int>()
+    val gameBored = setUpBored()
+    val placeHolder = placeCoins()
 println(gameBored)
 
 }
@@ -31,9 +35,22 @@ when (userInput) {
 
 
 }
-//Setting up the amount of spaces and coins on the bored 
+//Setting up the amount of spaces
 fun setUpBored(): MutableList<String> {
     val gameBored = mutableListOf<String>()
     for (i in 1..NUMSPACES) gameBored.add(EMPTY)
     return gameBored
 }
+//putting the coins in random spaces and checking if it's empty
+fun placeCoins (gameBored: MutableList<String>)  {
+val coin= gameBored.random()
+ when {
+     coin.equals(EMPTY) -> gameBored.add("o")
+     else ->
+
+ }
+}
+
+// Place coins in empty places until there are the same amount of coins as NUMCOINS if there are less the loop continues if there are the same amount the loop breaks
+// have one coin that stops (breaks) the game as soon as it is removed from the bored
+//have alternating turns between two human players (possibly add player scores)
